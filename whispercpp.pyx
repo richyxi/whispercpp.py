89cae3b69140cb81914f090bbbfcc87a19d9d628
richyxi <47098907+richyxi@users.noreply.github.com>
2024-03-05 20:20:55 -0600

    Update whispercpp.pyx

--- whispercpp.pyx
+++ whispercpp.pyx
@@ -16,7 +16,7 @@ cimport numpy as cnp
 cdef int SAMPLE_RATE = 16000
 cdef char* TEST_FILE = 'test.wav'
 cdef char* DEFAULT_MODEL = 'tiny'
-cdef char* LANGUAGE = b'en'
+cdef char* LANGUAGE = b'es'
 cdef int N_THREADS = os.cpu_count()
 
 MODELS = {
